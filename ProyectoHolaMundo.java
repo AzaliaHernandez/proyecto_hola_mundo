f0d8c78ef80d3a62f229477aa7d0ab11be7fbad5
AzaliaHG <fatimahrdzguz07@gmail.com>
2026-05-10 10:51:55 -0600

    Adding 2 numbers

--- ProyectoHolaMundo.java
+++ ProyectoHolaMundo.java
@@ -1,9 +1,22 @@
+
+import java.util.Scanner;
+
 public class ProyectoHolaMundo {
     public static void main(String[] args) {
-        
-        System.out.println("Hola Mundo");
-        System.out.println("Bienvenidos a Java");
-        System.out.println("Y a Git/GitHub");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Hola Mundo");
+            System.out.println("Bienvenidos a Java");
+            System.out.println("Y a Git/GitHub");
+            
+            System.out.println("-------------------");
+            System.out.println("Ingresar el primer numero");
+            double num1=scanner.nextDouble();
+            System.out.print("Ingrese el segundo número: ");
+            double num2 = scanner.nextDouble();
+            
+            double suma =num1+num2;
+            System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+        }
     }
     
 }
