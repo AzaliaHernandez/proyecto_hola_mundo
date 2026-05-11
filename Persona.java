91b874a5121d3445f27985117cdb9f75579e91be
AzaliaHG <fatimahrdzguz07@gmail.com>
2026-05-10 20:19:52 -0600

    Adding class Person

---
+++ Persona.java
@@ -0,0 +1,40 @@
+public class Persona {
+    private String Nombre;
+    private int  Edad;
+    private String APaterno;
+    private String AMaterno;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getAPaterno() {
+        return APaterno;
+    }
+
+    public void setAPaterno(String APaterno) {
+        this.APaterno = APaterno;
+    }
+
+    public String getAMaterno() {
+        return AMaterno;
+    }
+
+    public void setAMaterno(String AMaterno) {
+        this.AMaterno = AMaterno;
+    }
+
+    
+}
